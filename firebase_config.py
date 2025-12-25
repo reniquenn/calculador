08aa1d51522b265ed9daa4d46c78e79266fc0134
reniquenn <daniel.quintana2025@hotmail.com>
2025-12-25 17:46:47 -0300

    1
    1

--- firebase_config.py
+++ firebase_config.py
@@ -1,14 +1,20 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
+import streamlit as st
+import json
 import os
 
-# Ruta absoluta segura
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-KEY_PATH = os.path.join(BASE_DIR, "firebase_key.json")
-
-# Evita inicializar dos veces
 if not firebase_admin._apps:
-    cred = credentials.Certificate(KEY_PATH)
+    if "FIREBASE_KEY" in st.secrets:
+        # Modo Streamlit Cloud
+        cred_dict = json.loads(st.secrets["FIREBASE_KEY"])
+        cred = credentials.Certificate(cred_dict)
+    else:
+        # Modo local
+        BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+        KEY_PATH = os.path.join(BASE_DIR, "firebase_key.json")
+        cred = credentials.Certificate(KEY_PATH)
+
     firebase_admin.initialize_app(cred)
 
 db = firestore.client()
